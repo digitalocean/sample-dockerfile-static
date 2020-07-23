df9fcdde204b6bc413912d42aceaaf068efe607e
Kamal Nasser <kamal@digitalocean.com>
2020-07-23 18:43:27 +0300

    Dockerfile.static

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@
  */
 
 import React from "react"
+import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -25,6 +26,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Helmet>
+        <link rel="stylesheet" href={'/example.css'} />
+      </Helmet>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
