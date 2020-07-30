8348252cb099e28fd57bacfb184e72a03b644b98
Kamal Nasser <kamal@digitalocean.com>
2020-07-30 03:31:33 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site running on App Platform! <strong>This site was built within a Docker container!</strong></p>
+    <h1>Yaa yeeet</h1>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
