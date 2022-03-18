0dc0dd0fae78d3128c2d43f7e0d33a2e3ade9ad2
James Thigpen <james.r.thigpen@gmail.com>
2022-03-17 18:10:37 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site running on App Platform! <strong>This site was built within a Docker container!</strong></p>
-    <p>Now go build something great.</p>
+    <h1>Hi Ritz!</h1>
+    <p>Ritz is a total butthole!<strong>ほんとに!</strong></p>
+    <p>Pbbbbbt</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
